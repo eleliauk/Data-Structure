042c67ae454cc4b565388df7c7f96da1266d18de
eleliauk <2831336720@qq.com>
2024-09-23 17:33:30 +0800

    feat:chapter1

--- chapter1/unit1.cpp
+++ chapter1/unit1.cpp
@@ -1,8 +1,19 @@
 //
 // Created by 单禹嘉 on 24-9-23.
-//
+//10.4.1 含并交和差运算的集合类型
 #include <iostream>
 using namespace std;
-int main() {
-    cout << "Hello, World!" << endl;
-}
+//元素类型 结点类型和指针类型
+typedef int ElementType;
+typedef struct NodeType {
+    ElementType data;
+    NodeType * next;
+}NodeType,*LinkType;
+//有序表类型
+typedef struct {
+    LinkType head,tail;
+    int size;
+    int curpos;
+    LinkType current;
+}OrderedList;
+
